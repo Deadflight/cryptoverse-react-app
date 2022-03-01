4958d33c5d1fd9aba1fb0f23263fca4bb08664f5
Carlos Correa <33291776+Deadflight@users.noreply.github.com>
2022-03-01 12:43:30 -0400

    remove exchange cause belong to premium

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,12 +44,12 @@ const Navbar = () => {
           <Menu.Item icon={<HomeOutlined/>}>
             <Link to="/">Home</Link>
           </Menu.Item>
-          {/* <Menu.Item icon={<FundOutlined/>}>
+          <Menu.Item icon={<FundOutlined/>}>
             <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-          </Menu.Item> */}
-          <Menu.Item icon={<MoneyCollectOutlined/>}>
-            <Link to="/exchanges">Exchanges</Link>
           </Menu.Item>
+          {/* <Menu.Item icon={<MoneyCollectOutlined/>}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item> */}
           <Menu.Item icon={<BulbOutlined/>}>
             <Link to="/news">News</Link>
           </Menu.Item>
